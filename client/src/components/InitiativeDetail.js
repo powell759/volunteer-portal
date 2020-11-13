25571d1ce38700889c681ff9c2efcf50d70b7fba
ecaoile <unfie555@gmail.com>
2020-11-12 23:11:53 -0800

    added check to null end date string when there is no end date

--- client/src/components/InitiativeDetail.js
+++ client/src/components/InitiativeDetail.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Button, Container, Row, Col, Card, CardDeck } from 'react-bootstrap';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import LoadingSpinner from './LoadingSpinner';
 
@@ -65,11 +65,15 @@ function InitiativeDetail(props) {
           'default',
           timeOpts
         ).format(startDate);
-        const endDate = new Date(evt['end_datetime']);
-        const endDateStr = endDate.toLocaleDateString(undefined, dateOpts);
-        const endTimeStr = new Intl.DateTimeFormat('default', timeOpts).format(
-          endDate
-        );
+        const endDate = evt['end_datetime']
+          ? new Date(evt['end_datetime'])
+          : null;
+        const endDateStr = endDate
+          ? endDate.toLocaleDateString(undefined, dateOpts)
+          : null;
+        const endTimeStr = endDate
+          ? new Intl.DateTimeFormat('default', timeOpts).format(endDate)
+          : null; // dunno if we're gonna use this but leaving it here
         const dateElem =
           startDateStr == endDateStr ? (
             <>
@@ -81,7 +85,8 @@ function InitiativeDetail(props) {
             <>
               <h3 className="sm-copy-blue">Begins at {startTimeStr}</h3>
               <h3 className="sm-copy-blue">
-                {startDateStr} - {endDateStr}
+                {startDateStr}
+                {endDateStr ? ` - ${endDateStr}` : null}
               </h3>
             </>
           );
